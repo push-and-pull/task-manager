27fa24b2bb32eea1965b0e6663c94cd72319c09a
Maxim Schepelin <m.schepelin@gmail.com>
2014-10-05 17:48:25 +0300

    review remarks

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -8,17 +8,21 @@ from users.models import User
 
 class Task(models.Model):
 
-    TASK_STATUS = (
-        (0, 'open'),
-        (1, 'resolved')
-    )
+    class TASK_STATUS(object):
+        OPEN = 0
+        RESOLVED = 1
+
+        VALUES = {
+            OPEN: 'open',
+            RESOLVED: 'resolved'
+        }
 
     created_by = models.ForeignKey(to=User, verbose_name='Created by')
     title = models.CharField(verbose_name='Title', max_length=200)
     description = models.TextField(verbose_name='Description', blank=True)
-    status = models.IntegerField(choices=TASK_STATUS)
+    status = models.IntegerField(choices=TASK_STATUS.VALUES.items())
 
-    created_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+    created_at = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField()
 
     def __unicode__(self):
