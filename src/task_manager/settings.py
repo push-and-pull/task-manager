295fa31a94d3ab665b8c7672b7c3be44c573f3db
Maxim Schepelin <m.schepelin@gmail.com>
2014-10-05 13:28:40 +0300

    add import from settings_local

--- src/task_manager/settings.py
+++ src/task_manager/settings.py
@@ -81,3 +81,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    from settings_local import *
+except ImportError:
+    raise ImproperlyConfigured(
+        'You must define local settings. '
+        'Run: cp settings_local.tmpl settings_local.py'
+    )
