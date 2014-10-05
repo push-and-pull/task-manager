33fde2265a4e78fe220fe3eaa05a06982012795c
Maxim Schepelin <m.schepelin@gmail.com>
2014-10-05 16:01:15 +0300

    start tasks app

--- src/task_manager/settings.py
+++ src/task_manager/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'task_manager',
+    'tasks',
 )
 
 MIDDLEWARE_CLASSES = (
