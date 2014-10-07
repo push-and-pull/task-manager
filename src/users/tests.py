fb34bfd79d662edb8c77e339c16e4273eab62661
Effessio <trapeznikoff@yandex.ru>
2014-10-08 00:38:31 +0400

    battle against tests is still far from victory. However, we won't give up

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,16 +1,25 @@
 from django.test import TestCase
 from models import User
 from forms import UserRegisterForm
-# Create your tests here.
+from django.test import Client
 
 
 class UserRegisterTestCase(TestCase):
     def setUp(self):
-        User.objects.create_user(email='user@mail.ru', password='secret')
+        user = User.objects.create_user(email='user@mail.ru', password='secret')
+        self.user = user
 
     def test_form_existing_email(self):
-        user = User.objects.get()
-        form_data = {'email': user.email, 'password': '123', 'confirm_password': '123'}
-        print user.email
-        response = UserRegisterForm(data=form_data)
-        self.assertFormError(response, 'form', 'something', 'This field is required.')
+        c = Client()
+        form = UserRegisterForm(instance=self.user)
+
+        response = c.post('user/sign_in', {'email': self.user.email})
+
+        self.assertFormError(response, 'form', 'email', 'User with this email already exists')
+
+    def test_form_valid_data(self):
+        initial_count = User.objects.count()
+        c = Client()
+        c.post('/user/sign_in', {'email': 'f123@mail.ru', 'password': '123',
+                                 'confirm_password': '123'})
+        self.assertEqual(User.objects.count(), initial_count + 1)
